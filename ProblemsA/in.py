fbbd6585d9a767ff8c0142ed0cc2d0610a064679
Johan769 <remnyachizot2015@gmail.com>
2020-10-09 11:50:26 -0500

    Subiendo los archivos con sus nombres correctos

---
+++ ProblemsA/in.py
@@ -0,0 +1,16 @@
+t=int(input())
+while(t>0):
+    n=int(input())
+    l=[int(s) for s in input().split()]
+    s=c=0
+    for i in range(n):
+        if(s==0 and l[i]<0):
+            c+=abs(l[i])
+        else:
+            s+=l[i]
+        if(s<0):
+            c+=abs(s)
+            s=0
+    print(c)
+                
+    t-=1
